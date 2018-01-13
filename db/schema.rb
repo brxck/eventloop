8571a00f6d20e4778ac4e042b1073cbd37b7c504
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-13 11:12:43 -0700

    Add attendances model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112161050) do
+ActiveRecord::Schema.define(version: 20180113173650) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "attended_event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brock = User.create(name: 'Brock', email: 'brxck@test.com', password: 'test',
+                   password_confirmation: 'test')
+
+tori = User.create(name: 'Tori', email: 'toast@test.com', password: 'test',
+            password_confirmation: 'test')
+
+appa = User.create(name: 'Appa', email: 'moocat@test.com', password: 'test',
+            password_confirmation: 'test')
+
+brock.events.build(name: 'Riot', location: 'In the streets', datetime: DateTime.now).save
+brock.events.build(name: 'Afterparty', location: 'My place', datetime: DateTime.now).save
